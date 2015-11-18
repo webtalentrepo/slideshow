956358a9be9335910e45d539e3320918357a846c
freelancer <polaris.tiger803@yandex.com>
2015-11-18 10:47:34 +0200

    Fix issues. 1.) Active Period – Should display dates as MM/DD/YYYY 2.) Clicking on the Active Period should bring up the Calendar with current start and end dates being selected rather than the calendar changes the From and To dates to today’s date 3.) Schedule page should show full-thumbnail of the image/video so you easily know what image you’re working with. 4.) Apply button should be named Save 5.) Apply button should save and return you back to the Gallery

--- scripts/controllers/schedule.js
+++ scripts/controllers/schedule.js
@@ -8,78 +8,95 @@
  * Controller of the realApp
  */
 angular.module('realApp')
-        .controller('ScheduleCtrl', function ($scope, $http, $window) {
+		.controller('ScheduleCtrl', function ($scope, $http, $window) {
 
-            if ($window.sessionStorage.login !== "success") {
-                $window.location.href = '#/login';
-            }
+			if ($window.sessionStorage.login !== "success") {
+				$window.location.href = '#/login';
+			}
 
-            //file id getting
-            var buf = $window.location.href;
-            $scope.bufAry = buf.split("?");
-            if ($scope.bufAry[1]) {
-                $scope.bufAry1 = $scope.bufAry[1].split("=");
-                if ($scope.bufAry1[0] === "id") {
-                    $scope.file_id = $scope.bufAry1[1];
-                    if ($scope.file_id) {
-                        console.log($scope.file_id);
-                    }
-                }
-            }
-            // file schedule data getting
-            $scope.schedule_data = [];
-            var data = {
-                file_id: $scope.file_id
-            };
-            var request = $http({
-                method: "post",
-                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-                url: HOST_DIRECTORY + "get_schedule",
-                data: $.param(data)
-            });
-            request.success(
-                    function (html) {
-                        if (html.result === "YES") {
-                            $scope.schedule_data = html.data;
-                            console.log($scope.schedule_data);
-                            // Configuration Form
-                            if($scope.schedule_data['title']){
-                                $scope.file_title = $scope.schedule_data['title'];
-                            }
-                            else {
-                                $scope.file_title = $scope.schedule_data['origin_name'];
-                            }
-                            $scope.dwell_time = Number($scope.schedule_data['stay_time']);
-                            $scope.start_effect = $scope.schedule_data['start_effect'];
-                            $scope.end_effect = $scope.schedule_data['end_effect'];
-                            $scope.active_time = $scope.schedule_data['begin_date'].replace(/-/g,"/") + "-" + $scope.schedule_data['end_date'].replace(/-/g,"/");
-                            console.log($scope.active_time);
-                        }
-                    }
-            );
-            // time picker control
-            $('input[name="daterange"]').daterangepicker();
-            // schedule upadte function    
-            $scope.updateClk = function () {
-                var param = {
-                    'user_id': $scope.schedule_data['user_id'],
-                    'file_id': $scope.schedule_data['id'],
-                    'file_title': $scope.file_title,
-                    'dwell_time': $scope.dwell_time,
-                    'start_effect': $scope.start_effect,
-                    'end_effect': $scope.end_effect,
-                    'active_time': scheduleForm.elements["daterange"].value
-                };
-                console.log(param);
-                var request = $http({
-                    method: "post",
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-                    url: HOST_DIRECTORY + "schedule_update",
-                    data: $.param(param)
-                });
-            }
-            $scope.back = function(){
-                $window.location.href = "#/";
-            }
+			//file id getting
+			var buf = $window.location.href;
+			$scope.bufAry = buf.split("?");
+			if ($scope.bufAry[1]) {
+				$scope.bufAry1 = $scope.bufAry[1].split("=");
+				if ($scope.bufAry1[0] === "id") {
+					$scope.file_id = $scope.bufAry1[1];
+					if ($scope.file_id) {
+//						console.log($scope.file_id);
+					}
+				}
+			}
+			// file schedule data getting
+			$scope.schedule_data = [];
+			var data = {
+				file_id: $scope.file_id
+			};
+			var request = $http({
+				method: "post",
+				headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+				url: HOST_DIRECTORY + "get_schedule",
+				data: $.param(data)
+			});
+			$scope.sdate = '';
+			$scope.edate = '';
+			request.success(
+					function (html) {
+						if (html.result === "YES") {
+							$scope.schedule_data = html.data;
+							$scope.thumb_url = $scope.schedule_data['thumb'];
+//							console.log($scope.schedule_data);
+							// Configuration Form
+							if ($scope.schedule_data['title']) {
+								$scope.file_title = $scope.schedule_data['title'];
+							}
+							else {
+								$scope.file_title = $scope.schedule_data['origin_name'];
+							}
+							$scope.dwell_time = Number($scope.schedule_data['stay_time']);
+							$scope.start_effect = $scope.schedule_data['start_effect'];
+							$scope.end_effect = $scope.schedule_data['end_effect'];
+							var bdateary = $scope.schedule_data['begin_date'].split('-');
+							var edateary = $scope.schedule_data['end_date'].split('-');
+							$scope.sdate = (bdateary[1] + '/' + bdateary[2] + '/' + bdateary[0]);
+							$scope.edate = (edateary[1] + '/' + edateary[2] + '/' + edateary[0]);
+							$scope.active_time = $scope.sdate + "-" + $scope.edate;
+							// time picker control
+							$('input[name="daterange"]').daterangepicker({
+								format: 'MM/DD/YYYY',
+								startDate: $scope.sdate,
+								endDate: $scope.edate
+							}, function (start, end, label) {
+							});
+//							console.log($scope.active_time);
+						}
+					}
+			);
+			// schedule upadte function    
+			$scope.updateClk = function () {
+				var param = {
+					'user_id': $scope.schedule_data['user_id'],
+					'file_id': $scope.schedule_data['id'],
+					'file_title': $scope.file_title,
+					'dwell_time': $scope.dwell_time,
+					'start_effect': $scope.start_effect,
+					'end_effect': $scope.end_effect,
+					'active_time': scheduleForm.elements["daterange"].value
+				};
+//				console.log(param);
+				var request = $http({
+					method: "post",
+					headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+					url: HOST_DIRECTORY + "schedule_update",
+					data: $.param(param)
+				});
+				request.success(function () {
+//					$window.localStorage.clear();
+					$window.localStorage.removeItem('local_data');
+					$window.location.href = "#/";
+				});
+			};
+			$scope.back = function () {
+				$window.location.href = "#/";
+			};
 
-        });
+		});
